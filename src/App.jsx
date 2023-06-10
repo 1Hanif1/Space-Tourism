0440211870536ec742ece3c7462d0e43c3463294
hanif <hanifmohammed869@gmail.com>
2023-06-10 10:00:53 +0530

    Working on Dynamic Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import * as React from "react";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import "./App.css";
+import Destination from "./components/Destination";
+
+const changeTab = function (tabName) {
+  console.log(tabName);
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/home" />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/destination",
+    element: <Destination />,
+  },
+]);
 
 function App() {
   return (
     <main>
-      <Navbar />
-      <Home />
+      <Navbar changeTabFunction={changeTab} />
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </main>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import logo from "../assets/shared/logo.svg";
 import hamburger from "../assets/shared/icon-hamburger.svg";
 import close from "../assets/shared/icon-close.svg";
 import classes from "./Navbar.module.css";
-export default function Navbar() {
+export default function Navbar(props) {
+  const { changeTabFunction } = props;
+  const url = window.location.href.split("/")[3];
+  const nav = document.querySelectorAll("nav > ul > li");
+  let [activeTab, setActiveTab] = useState("");
+  useEffect(() => {
+    nav.forEach((li) => li.classList.remove("active"));
+    switch (url) {
+      case "home":
+        setActiveTab("home");
+        break;
+      case "destination":
+        setActiveTab("destination");
+        break;
+      case "crew":
+        setActiveTab("crew");
+        break;
+      case "technology":
+        setActiveTab("technology");
+        break;
+      default:
+        break;
+    }
+  }, [url, nav]);
+
+  useEffect(() => {
+    changeTabFunction(activeTab);
+  }, [activeTab, changeTabFunction]);
+
   const openMenu = function () {
     const navbar = document.querySelector("#navbar");
     navbar.classList.add(classes.open);
@@ -25,10 +55,24 @@ export default function Navbar() {
         <div className={classes.close}>
           <img src={close} onClick={closeMenu} />
         </div>
-        <li>00 Home</li>
-        <li>01 Destination</li>
-        <li>02 Crew</li>
-        <li>03 Technology</li>
+        <li id="home" className={activeTab == "home" ? classes.active : null}>
+          00 Home
+        </li>
+        <li
+          id="destination"
+          className={activeTab == "destination" ? classes.active : null}
+        >
+          01 Destination
+        </li>
+        <li id="crew" className={activeTab == "crew" ? classes.active : null}>
+          02 Crew
+        </li>
+        <li
+          id="technology"
+          className={activeTab == "technology" ? classes.active : null}
+        >
+          03 Technology
+        </li>
       </ul>
     </nav>
   );
